a8f637064d29f237f64fe19b8b5c628190f08b85
mariakoshevets10 <mashakoshevets@mail.ru>
2022-11-29 18:12:19 +0300

    homework

---
+++ homework/homework1/foobar.rb
@@ -0,0 +1,17 @@
+a = print"введите число 1: "
+a = gets.chomp().to_i
+b = print"введите число 2: "
+b = gets.chomp().to_i
+def foobar(a, b)
+
+    if a == 20
+        puts (b)
+    elsif b == 20
+        puts (a)
+            
+    elsif
+        puts (a+b)
+    end
+end
+
+puts foobar a, b 
