ac555ba0dd6f59bfbf7c44f18119dbe0169961a0
mariakoshevets10 <mashakoshevets@mail.ru>
2022-11-29 15:57:55 +0300

    homework 1(greeting)

---
+++ homework/homework1/greeting.rb
@@ -0,0 +1,21 @@
+
+
+
+    puts("Введите свое имя: ")
+    name = gets.chomp()
+
+    puts("Введите фамилию: ")
+    surname = gets.chomp()
+
+    puts("Введите возраст: ")
+    age = gets.to_i()
+
+def greeting (name, surname, age)
+
+    puts "Привет, #{name} #{surname}."
+    age < 18 ? "Тебе меньше 18, но начать учиться программировать никогда не рано" : "Самое время заняться делом"
+   
+end
+    
+
+puts greeting name, surname, age
