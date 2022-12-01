dec0e99c162dd5b189441de6ab5304ca63e71cf0
mariakoshevets10 <mashakoshevets@mail.ru>
2022-12-01 21:04:29 +0300

    add homework1

---
+++ Homework/foobar.rb
@@ -0,0 +1,17 @@
+print"введите число 1: "
+b = gets.chomp().to_i
+print"введите число 2: "
+c = gets.chomp().to_i
+
+def foobar(b, c)
+
+    if b == 20
+        puts (c)
+    elsif c == 20
+        puts (b)        
+    elsif
+        puts (b+c)
+    end
+end
+
+puts foobar b, c
